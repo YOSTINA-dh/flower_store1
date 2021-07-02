eb52bc799efc5da0e638bfa784c15627a194fc78
YOSTINA-dh <yostinadesta@gmail.com>
2021-07-02 10:51:08 +0000

    started product template

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 
+
 # Create your views here.
 
 def all_products(request):
@@ -13,4 +14,3 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-    

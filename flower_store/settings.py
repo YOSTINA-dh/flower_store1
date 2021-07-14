ae191c73b13650c746cde85758fb402a1dcb514d
YOSTINA-dh <yostinadesta@gmail.com>
2021-07-14 20:23:02 +0000

    uppdate

--- flower_store/settings.py
+++ flower_store/settings.py
@@ -180,6 +180,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
@@ -189,7 +190,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'flowerstore-ms4'
-    AWS_S3_REGION_NAME = 'US East (Ohio) us-east-2'
+    AWS_S3_REGION_NAME = 'us-east-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
